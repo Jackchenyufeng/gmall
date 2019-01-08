bba53c8aba99378d19890e68e0c91711c5029cc0
jack <chenyufeng1991@aliyun.com>
2019-01-08 21:46:32 +0800

    general Mapper

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/UserService.java
@@ -0,0 +1,10 @@
+package com.atguigu.gmall.user.service;
+
+import com.atguigu.gmall.user.bean.UserInfo;
+
+import java.util.List;
+
+public interface UserService {
+    public List<UserInfo> getUserList();
+
+}
---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/iml/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.atguigu.gmall.user.service.iml;
+
+import com.atguigu.gmall.user.bean.UserInfo;
+import com.atguigu.gmall.user.mapper.UserInfoMapper;
+import com.atguigu.gmall.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    UserInfoMapper userInfoMapper;
+    @Override
+    public List<UserInfo> getUserList() {
+        //List<UserInfo> userInfos = userInfoMapper.selectUserList();
+        List<UserInfo> userInfos = userInfoMapper.selectAll();
+        return userInfos;
+    }
+}
